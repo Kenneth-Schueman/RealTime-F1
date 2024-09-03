6272e7daa2927241a66861022daa814106401e2b
Kenneth <kenschue@iastate.edu>
2024-09-03 12:11:15 -0500

    something

--- testFiles/mainTest.py
+++ testFiles/mainTest.py
@@ -4,7 +4,7 @@ import json
 year = input("Enter the year you are searching for.\nNote: The farthest available data is 2023.\nYear: ")
 
 # Check if the file in pastRaces/data exists with the year name
-data_file = os.path.join("pastRaces", "data", f"{year}.json")
+data_file = os.path.join("data", f"{year}.json")
 if not os.path.isfile(data_file):
      # Construct the path to the getMeetings file
     getMeetings = os.path.join("pastRaces", "getMeetings.py")
@@ -20,5 +20,14 @@ for i, meeting in enumerate(data, start=1):
     print(f"{i}. {meeting['meeting_name']}: {meeting['location']}, {meeting['country_name']} ({meeting['date_start']})")
 
 selectedMeeting = input("\nPlease select a meeting to display the details. (Example: 1)\nSelection: ")
+
+driver_file = os.path.join("data", "driverData.json")
+
+# Parse the driver_file
+with open(driver_file, 'r') as file:
+    driverData = json.load(file)
+
+for driver in driverData:
+    print(f"{driver['number']}. {driver['name']}")
 # Construct the path to the displayMeetings file
-displayMeetings = os.path.join("displayMeeting", "displayMeeting.py")
+#displayMeetings = os.path.join("displayMeeting", "displayMeeting.py")
