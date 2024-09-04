8bc51cdb8d1b52e6d9d3d7aae73dfa2b1dfb0fd6
Kenneth-Schueman <kenschue@iastate.edu>
2024-09-04 16:13:35 -0500

    testing

--- testFiles/mainTest.py
+++ testFiles/mainTest.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+#!!! For testing using parameters, year = 2023, meeting = 16 and driverNumber = 81
+
 year = input("Enter the year you are searching for.\nNote: The farthest available data is 2023.\nYear: ")
 
 # Check if the file in pastRaces/data exists with the year name
@@ -27,7 +29,19 @@ driver_file = os.path.join("data", "driverData.json")
 with open(driver_file, 'r') as file:
     driverData = json.load(file)
 
-for driver in driverData:
+for driver in driverData['drivers']:
     print(f"{driver['number']}. {driver['name']}")
+
+driverNumber = input("\nPlease select a driver to display the details. (Example: 1)\nSelection: ")
+
+# Check if the selected driver number is valid
+# valid_driver_numbers = [driver['number'] for driver in driverData['drivers']]
+# while driverNumber not in valid_driver_numbers:
+#     print("Invalid driver number. Please try again.")
+#     driverNumber = input("Selection: ")
+
+
+#
+os.system(f"python testFiles/testDisplay.py {data[int(selectedMeeting) - 1]['date_start']} {driverNumber}")
 # Construct the path to the displayMeetings file
 #displayMeetings = os.path.join("displayMeeting", "displayMeeting.py")
--- testFiles/testDisplay.py
+++ testFiles/testDisplay.py
@@ -1,8 +1,11 @@
 import pygame
 from urllib.request import urlopen
 import json
+import sys
 
-
+#Params: dateTime, driverNumber 
+dateTime = sys.argv[1]
+driverNumber = sys.argv[2]
 
 # Initialize Pygame
 pygame.init()
@@ -25,7 +28,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 screen.fill((0, 0, 0))
 
 # Draw a circle and animate it to F1 data from the API
-response = urlopen('https://api.openf1.org/v1/location?session_key=9161&driver_number=81&date<2023-09-16T13:03:35.200')
+print(f'https://api.openf1.org/v1/location?session_key=9161&driver_number={driverNumber}&date<{dateTime}')
+response = urlopen(f'https://api.openf1.org/v1/location?session_key=9161&driver_number={driverNumber}&date<{dateTime}')
 data = json.loads(response.read().decode('utf-8'))
 print(data)
 for item in data:
